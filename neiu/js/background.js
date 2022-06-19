2b4323d52596c84161ec29781e6d351c3520c95b
home <ec98721zzz@gmail.com>
2022-06-19 14:29:00 +0900

    ver3

--- neiu/js/background.js
+++ neiu/js/background.js
@@ -4,21 +4,33 @@ chrome.runtime.onMessage.addListener((message, sender, response) => {
 	//console.log(sender.tab.url);
 	
 	var domain= sender.tab.url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1];
-	//console.log(R);
+	//console.log(domain);
 	
 	chrome.storage.local.get('urls', function (items) {
-		const urls = Array.from(new Set(items.urls.split('`')));
+		//console.log(items.urls);
 		
-		for(var i=1;i<urls.length;i++)
-			
-			if(domain === urls[i].match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1]){
-				chrome.tabs.remove(sender.tab.id);
-				chrome.tabs.create({url: "public/blocked.html"});
+		const urls = Array.from(new Set(items.urls.split('`')));
+		//console.log(urls)
+		
+		for(var i=1; i <urls.length ;i++){
+			/*
+			console.log(domain);
+			console.log(urls);
+			console.log(i);
+			console.log(urls[i]);
+			console.log(urls[i].match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1])
+			*/
+			var url = urls[i].match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/);
+			if(url !== null){
+				//console.log(url)
+				if(domain === url[1]){
+					chrome.tabs.remove(sender.tab.id);
+					chrome.tabs.create({url: "public/blocked.html"});
+				}
 			}
+		}
 	});
 	
 	response();
 })
-
-
 
